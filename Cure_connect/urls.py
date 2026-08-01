69462267c24568b6d619ae329ecb994b8ee79993
saltymcguy <johnphiliposemodayil@gmail.com>
2026-08-01 13:58:57 +0530

    Refactor URL routing and update binary files
    - Updated `urls.py` to include new paths for home, patients, doctors, and appointments dashboards.
    - Modified various `.pyc` files in `Cure_connect`, `accounts`, `appointments`, `dashboard`, `doctors`, and `patients` directories to reflect recent changes in the codebase.
    - Added new compiled Python files for the `views` module in the `dashboard` app.

--- Cure_connect/urls.py
+++ Cure_connect/urls.py
@@ -1,6 +1,12 @@
 ﻿from django.contrib import admin
 from django.urls import path
+from dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.home, name='home'),
+    path('patients/', views.patient_dashboard, name='patients'),
+    path('doctors/', views.doctor_dashboard, name='doctors'),
+    path('appointments/', views.appointment_dashboard, name='appointments'),
 ]
