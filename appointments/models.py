23dbfb19140943f9971d6aa30f260bb40c8dff00
saltymcguy <johnphiliposemodayil@gmail.com>
2026-08-01 13:05:09 +0530

    Add appointment, doctor, and patient models with migrations

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from patients.models import Patient
+from doctors.models import Doctor
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
+    date = models.DateField()
+    time = models.TimeField()
+
+    STATUS = [
+        ("Pending", "Pending"),
+        ("Confirmed", "Confirmed"),
+        ("Completed", "Completed"),
+    ]
+
+    status = models.CharField(max_length=20, choices=STATUS, default="Pending")
+
+    def __str__(self):
+        return f"{self.patient} - {self.doctor}"
