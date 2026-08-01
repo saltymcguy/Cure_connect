abc8260d9219714f70fb32766f4d519f5380b939
vaishnaviupta14-commits <vaishnaviupta.14@gmail.com>
2026-08-01 11:53:46 +0530

    added models in models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ADMIN = "ADMIN"
+    DOCTOR = "DOCTOR"
+    PATIENT = "PATIENT"
+
+    ROLE_CHOICES = [
+        (ADMIN, "Administrator"),
+        (DOCTOR, "Doctor"),
+        (PATIENT, "Patient"),
+    ]
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=PATIENT
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
